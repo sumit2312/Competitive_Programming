fca5b15d40863383c0cf84144dd961c123841f35
Sumit Kumar <38119051+sumit2312@users.noreply.github.com>
2019-08-03 14:48:13 +0530

    Update and rename Merge two sorted array to Merge two sorted array.cpp

--- Array/Merge two sorted array.cpp
+++ Array/Merge two sorted array.cpp
@@ -34,8 +34,7 @@ using namespace std;
 #define debug2(x,y) cout << x << " " << y << endl;
 #define debug3(x,y,z) cout << x << " " << y << " " << z << endl;
 
-void mergeArrays(int arr1[], int arr2[], int n1, 
-							int n2, int arr3[]) 
+void mergeArrays(int arr1[], int arr2[], int n1,n2, int arr3[]) 
 { 
 	int i = 0, j = 0, k = 0; 
 
