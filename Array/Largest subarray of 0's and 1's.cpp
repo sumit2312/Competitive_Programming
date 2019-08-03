30a739a78b2ab9605e37b1fc058d0bfed1dbe0ef
Sumit Kumar <38119051+sumit2312@users.noreply.github.com>
2019-08-03 14:52:07 +0530

    Update Largest subarray of 0's and 1's.cpp

--- Array/Largest subarray of 0's and 1's.cpp
+++ Array/Largest subarray of 0's and 1's.cpp
@@ -5,13 +5,19 @@ maximum index difference.*/
 
 /*Here's an O(n)-time, O(n)-space algorithm. I'm not sure it's optimal, but it beats quadratic time.
 
-The basic idea is the following. Suppose that you scan from the left of the array to the right recording, at each step, the difference between the number of 1s and the number of 0s. If you write these values out at each step, you'll get something like this:
+The basic idea is the following. Suppose that you scan from the left of the array to the right recording, at each step,
+the difference between the number of 1s and the number of 0s. If you write these values out at each step, you'll get 
+something like this:
 
   1,   0,   1,    0,    0,   0,   0
 0,   1,   0,   1,    0,    -1,  -2,  -3
-If you have a sub array with the same number of 0s and 1s, then the net difference of 0s and 1s at the start of the subarray will equal the net number after the subarray. Therefore, this problem can be reframed as trying to find two equal values in the auxiliary array that are equal and as far apart as possible.
+If you have a sub array with the same number of 0s and 1s, then the net difference of 0s and 1s at the start of the
+subarray will equal the net number after the subarray. Therefore, this problem can be reframed as trying to find two
+equal values in the auxiliary array that are equal and as far apart as possible.
 
-The good news is that every entry in the array is between -n and +n, so you can make a 2n+1 element table and store in it the indices of the first and last time each number appears. From there, it's easy to find the longest range. Overall, this needs O(n) space and everything can be populated and searched in O(n) time.
+The good news is that every entry in the array is between -n and +n, so you can make a 2n+1 element table and store 
+in it the indices of the first and last time each number appears. From there, it's easy to find the longest range. 
+Overall, this needs O(n) space and everything can be populated and searched in O(n) time.
 */
 
 
