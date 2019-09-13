79374cb0e073468e19084cf41bbafb8828cbb7f7
Sumit Kumar <38119051+sumit2312@users.noreply.github.com>
2019-09-13 20:34:23 +0530

    Update Coin Change ( min ) .cpp

--- DP/Coin Change ( min ) .cpp
+++ DP/Coin Change ( min ) .cpp
@@ -1,7 +1,10 @@
 // https://leetcode.com/problems/coin-change/
 
+/* 
 
-You are given coins of different denominations and a total amount of money amount. Write a function to compute the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+You are given coins of different denominations and a total amount of money amount. Write a function
+to compute the fewest number of coins that you need to make up that amount. If that amount of money
+cannot be made up by any combination of the coins, return -1.
 
 Example 1:
 
@@ -13,6 +16,7 @@ Example 2:
 Input: coins = [2], amount = 3
 Output: -1
 
+*/
 
 class Solution {
 public:
