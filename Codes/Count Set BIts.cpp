ed3da4cab088a7db9a44c357445daa6fe44583a2
Sumit Kumar <38119051+sumit2312@users.noreply.github.com>
2019-09-17 22:55:33 +0530

    Create Count Set BIts.cpp

---
+++ Codes/Count Set BIts.cpp
@@ -0,0 +1,11 @@
+unsigned int countSetBits(unsigned int n) 
+{ 
+unsigned int count = 0; 
+while (n) 
+{ 
+    count += n & 1; 
+    n >>= 1; 
+} 
+return count; 
+} 
+/*******************************************************/
