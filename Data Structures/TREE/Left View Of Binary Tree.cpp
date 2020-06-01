34aca2def050561f933598b78cee89e843fdf008
Sumit Kumar <38119051+sumit2312@users.noreply.github.com>
2020-06-01 22:53:17 +0530

    Update Left View Of Binary Tree.cpp

--- Data Structures/TREE/Left View Of Binary Tree.cpp
+++ Data Structures/TREE/Left View Of Binary Tree.cpp
@@ -30,3 +30,32 @@ void leftView(Node *root)
     int mx_level=0;
     leftView(root,1,&mx_level);
 }
+
+/* OR*/
+
+void leftView(Node *root)
+{
+   if(root == NULL){
+       return ;
+   }
+    queue<Node*> q;
+    q.push(root);
+    while(!q.empty()){
+       int sz = q.size();
+       for(int i=0;i<sz;i++){
+           Node* tp = q.front();
+           q.pop();
+           if(i==0){
+               cout<<tp->data<<" ";
+           }
+           if(tp->left){
+               q.push(tp->left);
+           }
+           if(tp->right){
+               q.push(tp->right);
+           }
+       }
+    }
+}
+
+
